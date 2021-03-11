99052cda79fb6d87955dc09a6608759fa760f263
HancharykNastassia <goncharik.anastasia@mail.ru>
2021-03-11 15:39:27 +0300

    feat: add colution to count cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function countCats(backyard) {
+  if ((backyard.length == 0) || backyard === undefined) return 0;
+  let catNumber = 0;
+  backyard.forEach(level => {
+    level.forEach(cat => {
+      if (cat === "^^") catNumber = catNumber + 1;
+    })
+  })
+
+  return catNumber;
+}
